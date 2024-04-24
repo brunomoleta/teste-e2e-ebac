301a340740ada2737359798a21a45bb8c68a79bb
Bruno Moleta <brunomoleta@pm.me>
2024-04-24 12:44:43 -0300

    🧹 Formatação do projeto usando Prettier

--- cypress.config.js
+++ cypress.config.js
@@ -5,6 +5,7 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl: "http://lojaebac.ebaconline.art.br/"
+    baseUrl: "http://lojaebac.ebaconline.art.br/",
+    execTimeout: 8 * 1000,
   },
 });
