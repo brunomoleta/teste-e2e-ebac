b90f074fc615f3f1e785b005c90224308b21d5ec
Bruno Moleta <brunomoleta@pm.me>
2024-05-07 14:05:45 -0300

    :config: webpack cucumber setup
    setup cypress.config.js properly so that only .feature files are run by Cypress.

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,43 @@
 const { defineConfig } = require("cypress");
+const webpack = require("@cypress/webpack-preprocessor");
+const {
+  addCucumberPreprocessorPlugin,
+} = require("@badeball/cypress-cucumber-preprocessor");
+
+async function setupNodeEvents(on, config) {
+  await addCucumberPreprocessorPlugin(on, config);
+
+  on(
+    "file:preprocessor",
+    webpack({
+      webpackOptions: {
+        resolve: {
+          extensions: [".ts", ".js"],
+        },
+        module: {
+          rules: [
+            {
+              test: /\.feature$/,
+              use: [
+                {
+                  loader: "@badeball/cypress-cucumber-preprocessor/webpack",
+                  options: config,
+                },
+              ],
+            },
+          ],
+        },
+      },
+    }),
+  );
+  return config;
+}
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
     baseUrl: "http://lojaebac.ebaconline.art.br/",
     execTimeout: 8 * 1000,
+    specPattern: "**/*.feature",
+    setupNodeEvents,
   },
 });
