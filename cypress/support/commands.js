d0363073481dae44d9cd38a1cab861cdb47d309f
Bruno Moleta <brunomoleta@pm.me>
2024-05-07 14:03:18 -0300

    :broom: remove unnecessary commands

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,19 +1,3 @@
-import "./commands/preencherDados/preencherDadosUsuario.commands";
-import "./commands/login/login.commands";
-import "./commands/comprar/acessarProduto.commands";
-import "./commands/comprar/comprarProduto.commands";
-import "./commands/finalizar/finalizarCompra.commands";
-
-Cypress.Commands.add("enterInput", (id, texto) => {
-  cy.get(`#${id}`).type(texto, { log: false });
-});
-
-Cypress.Commands.add("verificarQuantidade", (quantidade, final) => {
-  if (final) {
-    cy.get(".order_item").should("have.length", quantidade);
-  } else {
-    cy.get(".cart_item").should("have.length", quantidade);
-  }
-});
+import "./commands/index.commands";
 
 import "@testing-library/cypress/add-commands";
--- cypress/support/commands/login/login.commands.js
+++ cypress/support/commands/login/login.commands.js
@@ -1,5 +1,12 @@
-Cypress.Commands.add("login", (usuario, senha) => {
-  cy.get("#username").type(usuario);
-  cy.get("#password").type(senha, { log: false });
-  cy.get(".woocommerce-form > .button").click();
+Cypress.Commands.add("acessarAreaLogin", () => {
+  cy.get('title="Login"').click();
+});
+
+Cypress.Commands.add("enterInput", (id, texto) => {
+  cy.get(`#${id}`).type(texto, { log: false });
+});
+
+Cypress.Commands.add("login", (email, password) => {
+  cy.enterInput("username", email);
+  cy.enterInput("password", `${password}{enter}`);
 });
