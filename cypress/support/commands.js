bca2ae8bb0f0e1853e105d3786bd12be9d4e780d
Bruno Moleta <brunomoleta@pm.me>
2024-05-22 11:42:12 -0300

    :recycle: refactor:import Commands

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,3 +1,5 @@
-import "./commands/login/login.commands.js";
-
-import "@testing-library/cypress/add-commands";
+import "./commands/forms/login.commands";
+import "./commands/forms/respostaForm.command";
+import "./commands/forms/areaLogin.commands";
+import "./commands/buyItem.command";
+import "./commands/helper.command";
